aba8ef2a354424cced61b3b7776fef757e69f9ff
mzfr <mehtab.zafar98@gmail.com>
2018-12-01 15:24:19 +0530

    Clean meetup.py

--- MeetUp/meetup_main.py
+++ MeetUp/meetup_main.py
@@ -5,7 +5,8 @@ from prettytable import PrettyTable
 
 def make_url(city, genre):
 
-    url = "https://www.meetup.com/find/events/" + genre.lower() + "/?allMeetups=true&radius=Infinity&userFreeform=+" + city.lower() + "+%2C+India&mcId=z1018091&mcName=" + city.lower() + "%2C+IN"
+    url = "https://www.meetup.com/find/events/" + genre.lower() + "/?allMeetups=true&radius=Infinity&userFreeform=+" + \
+        city.lower() + "+%2C+India&mcId=z1018091&mcName=" + city.lower() + "%2C+IN"
     get_html(url)
 
 
@@ -16,32 +17,19 @@ def get_html(url):
 
 
 def get_event_list(soup):
-    data = soup.find_all('li', class_="row event-listing clearfix doc-padding  ")
-
-    # print(len(data))
-
+    data = soup.find_all('li', class_="row event-listing clearfix doc-padding ")
     columns = ["S.No", "Group", "Time", "Event Name"]
     table = PrettyTable(columns)
 
-    groups, time, event = [], [], []
     for i in range(len(data)):
         try:
             if i == 5:
                 break
             new = data[i].find_all("a")
-            # print(i+1, new[0].find("time").contents[0], new[1].find("span").contents[0], new[-1].find("span").contents[0])
             table.add_row([i + 1, new[1].find("span").contents[0].encode(encoding='UTF-8', errors='strict'),
                            new[0].find("time").contents[0].encode(encoding='UTF-8', errors='strict'),
                            new[-1].find("span").contents[0].encode(encoding='UTF-8', errors='strict')])
-
         except:
-            pass
+            print("EXCEPTION")
 
-    # print(groups)
-    '''
-    for i in range(len(groups)):
-        if i==5:
-            break
-        table.add_row([i+1, groups[i][0], groups[i][1], groups[i][2]])
-    '''
     print(table)
