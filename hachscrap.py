a6cfe34b923513d1348a615d6b9c179cf612a6cf
Harkirat  Singh <harkirat.hira@gmail.com>
2018-12-01 15:56:08 +0530

    Update hachscrap.py

--- hachscrap.py
+++ hachscrap.py
@@ -7,7 +7,7 @@ data = {}
 param = {'/?username'='harkirat155','api_key'='63b3040dd99d6820d23626e658843d9042ca638a'}
 
 
-resp = requests.get('https://clist.py/api/v1/contest/', params=params)
+resp = requests.get('https://clist.by/api/v1/contest/', params=params)
 if resp.status_code != 200:
     # This means something went wrong.
 #    raise ApiError('GET /api/v1/contest {}'.format(resp.status_code))
